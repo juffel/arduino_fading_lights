f7d727b684b984f38c54cc4a6efa32e4c35092a5
Julian Dobmann <julian.dobmann@mailbox.org>
2017-10-08 17:54:05 +0200

    structify colors

--- sketch_kistenleuchte.ino
+++ sketch_kistenleuchte.ino
@@ -8,32 +8,38 @@
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXELCOUNT, PIN, NEO_GRB + NEO_KHZ800);
 
+struct color {
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
+};
+
 void setup() {
   strip.begin();
   strip.show();
 }
 
-// uint32_t yellowish = strip.Color(140, 60, 0);
-// uint32_t white = strip.Color(140, 100, 60);
+struct color yellowish = { 140, 60, 0 };
+struct color white = { 190, 150, 90 };
+struct color blue = { 0, 0, 190 };
 
 void loop() {
-  colorFade(1000, 140, 60, 0, 190, 150, 90);
-  colorFade(1000, 190, 150, 90, 140, 60, 0);
+  colorFade(1000, blue, white);
+  colorFade(1000, white, yellowish);
 }
 
-void colorFade(uint16_t delayTime, uint8_t r1, uint8_t g1, uint8_t b1, uint8_t r2, uint8_t g2, uint8_t b2) {
+void colorFade(uint16_t delayTime, color color1, color color2) {
   int up;
   for(int down = 100; down > 0; down--) {
     up = 100 - down;
 
-    uint8_t red   = ((r1 * down) + (r2 * up)) / 200;
-    uint8_t green = ((g1 * down) + (g2 * up)) / 200;
-    uint8_t blue  = ((b1 * down) + (b2 * up)) / 200;
+    uint8_t red   = ((color1.r * down) + (color2.r * up)) / 200;
+    uint8_t green = ((color1.g * down) + (color2.g * up)) / 200;
+    uint8_t blue  = ((color1.b * down) + (color2.b * up)) / 200;
 
     setAll(strip.Color(red, green, blue));
     delay(delayTime);
   }
-
 }
 
 void setAll(uint32_t color) {
